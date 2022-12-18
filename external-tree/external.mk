2c4f671ec3f8be4a3c9a0542befe8af4d3d6f721
farbius <farbius@protonmail.com>
2022-12-18 12:51:10 -0800

    adding fpgautil package

---
+++ external-tree/external.mk
@@ -0,0 +1,1 @@
+include $(sort $(wildcard $(BR2_EXTERNAL_MYDIR_PATH)/package/*/*.mk))
---
+++ external-tree/package/fpgautil/fpgautil.mk
@@ -0,0 +1,13 @@
+FPGAUTIL_VERSION = 1.0
+FPGAUTIL_SITE = $(BR2_EXTERNAL_MYDIR_PATH)/package/fpgautil
+FPGAUTIL_SITE_METHOD = local
+
+define FPGAUTIL_BUILD_CMDS
+	$(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D)
+endef
+
+define FPGAUTIL_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/fpgautil $(TARGET_DIR)/usr/bin
+endef
+
+$(eval $(generic-package))
